96fd82891964ce350b1299cc4c59032277f759e8
Mosby7 <kfnpfeffer@gmail.com>
2015-10-29 05:14:55 +0100

    profile basics

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,12 @@ app.config(function($routeProvider, $locationProvider) {
             activetab : 'top',
             title : 'Top 100'
         })
+        .when('/profile', {
+            templateUrl : 'assets/templates/profile.html',
+            controller  : 'profileController',
+            activetab : 'profile',
+            title : 'Votre profilwa'
+        })
         .when('/login', {
             templateUrl : 'assets/templates/login.html',
             controller  : 'loginController',
